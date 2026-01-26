5b566534191781a077dda5f7a5017ca4462a7ff2
shubhamojha1 <85477752+shubhamojha1@users.noreply.github.com>
2026-01-26 15:17:50 +0000

    Deploying to gh-pages from @ shubhamojha1/shubhamojha1.github.io@920f250719f755408996ed4396df6fc881c96128 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,7 +65,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-how-some-projects-use-multiple-programming-languages",
+            },{id: "post-slowloris-the-silent-connection-killer",
+        
+          title: "Slowloris - The Silent Connection Killer",
+        
+        description: "A deep dive into Slowloris HTTP attacks - how they work, why they&#39;re dangerous, and how to defend against them",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/slowloris/";
+          
+        },
+      },{id: "post-how-some-projects-use-multiple-programming-languages",
         
           title: "How some projects use Multiple Programming Languages",
         
@@ -362,17 +373,6 @@ ninja.data = [{
             window.location.href = "/blog/2022/giscus-comments/";
           
         },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
       },{id: "post-a-post-with-redirect",
         
           title: "a post with redirect",
@@ -538,14 +538,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%63%6F%6E%74%61%63%74.%73%68%75%62%68%61%6D%6F%6A%68%61@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/shubhamojha1", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/shubhamojha1", "_blank");
         },
       },{
         id: 'social-rss',
@@ -560,13 +567,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
